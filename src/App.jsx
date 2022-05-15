b51c340f4f9c20ea044f951d0f85e00cd85deac6
SiriusLL <sirius.love.light@gmail.com>
2022-05-14 23:42:32 -0600

    connect wallet with thirdweb

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import { useAddress, useMetamask } from "@thirdweb-dev/react";
+
 const App = () => {
+  // thirdweb Hooks
+  const address = useAddress();
+  const connectWithMetamask = useMetamask();
+  console.log("Address", address);
+
+  // wallet connect
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to My DAO</h1>
+        <button onClick={connectWithMetamask} className="btn-hero">
+          Connect your wallet
+        </button>
+      </div>
+    );
+  }
+
+  // already connected
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>** wallet connected, now what!</h1>
     </div>
   );
 };
