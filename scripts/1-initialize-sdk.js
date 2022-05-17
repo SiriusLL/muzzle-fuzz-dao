aa00631839262b2e729bfb8d4072efe537100df4
SiriusLL <sirius.love.light@gmail.com>
2022-05-16 23:13:54 -0600

    save

--- scripts/1-initialize-sdk.js
+++ scripts/1-initialize-sdk.js
@@ -1,9 +1,11 @@
 import { ThirdwebSDK } from "@thirdweb-dev/sdk";
+import ethers from "ethers";
 
+// Importing and configuring our .env file that we use to securely store our environment variables
 import dotenv from "dotenv";
-import { ethers } from "ethers";
 dotenv.config();
 
+// Some quick checks to make sure our .env is working.
 if (!process.env.PRIVATE_KEY || process.env.PRIVATE_KEY === "") {
   console.log("🛑 Private key not found.");
 }
@@ -16,22 +18,22 @@ if (!process.env.WALLET_ADDRESS || process.env.WALLET_ADDRESS === "") {
   console.log("🛑 Wallet Address not found.");
 }
 
+// RPC URL, we'll use our Alchemy API URL from our .env file.
 const provider = new ethers.providers.JsonRpcProvider(
   process.env.ALCHEMY_API_URL
 );
-
+// Your wallet private key. ALWAYS KEEP THIS PRIVATE, DO NOT SHARE IT WITH ANYONE, add it to your .env file and do not commit that file to github!
 const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-
 const sdk = new ThirdwebSDK(wallet);
 
 (async () => {
   try {
     const address = await sdk.getSigner().getAddress();
-    console.log("👋 SDK initialized by address:", address);
+    console.log("SDK initialized by address:", address);
   } catch (err) {
     console.error("Failed to get apps from the sdk", err);
     process.exit(1);
   }
 })();
 
-export default sdk;
+// We are exporting the init
